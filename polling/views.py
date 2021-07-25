0aff3c120c65b4be53f48e18b6dba0a9a57c2449
Steve-Dusty <kuantingk2@gmail.com>
2021-07-24 21:35:37 -0700

    added results

--- polling/views.py
+++ polling/views.py
@@ -1,11 +1,12 @@
 import datetime
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.forms import inlineformset_factory
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 from .forms import QuestionForm, ChoiceForm, VoteForm
 from .models import Question, Choice
 
@@ -39,8 +40,8 @@ def vote(request, question_id):
             selected_choice = form.cleaned_data['choice']
             selected_choice.votes += 1
             selected_choice.save()
-            # return HttpResponseRedirect('polling:results', question_id=question_id)
-            return redirect('/')
+            return HttpResponseRedirect(reverse('results', args=(question.id,)))
+
 
         else:
             messages.error(request, "You must select an choice!")
@@ -84,8 +85,12 @@ def createchoice(request, question_id):
 
 
 def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'results.html', {'question': question})
+    question = Question.objects.get(pk=question_id)
+    # MUST FILTER THIS OUT CORRECTLY TO CORRESPONDING QUESTIONS
+    choices = Choice.objects.all()
+    context = {'question': question, 'choices': choices}
+    return render(request, 'results.html', context)
+
 
 
 """
