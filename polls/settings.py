cc6a7ade1f650b8c15158682aa25c9785f967653
Steve-Dusty <kuantingk2@gmail.com>
2021-07-26 20:13:05 -0700

    added default to databases environs

--- polls/settings.py
+++ polls/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'polls.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.dj_db_url('DATABASE_URL')
+    'default': env.dj_db_url('DATABASE_URL', default="sqlite://")
 }
 
 
